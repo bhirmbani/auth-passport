49ce8be2a5c0528205ef6c29c0f569fda9284f23
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-26 16:29:14 +0700

    book controller

---
+++ controllers/book.js
@@ -0,0 +1,48 @@
+const Book = require('../models/book');
+const methods = {};
+
+methods.add = (req, res, next) => {
+  Book.create(req.body, (err, book) => {
+    if (err) throw err;
+    res.send(book);
+  });
+}
+
+methods.gets = (req, res, next) => {
+  Book.find({}, (err, books) => {
+    if (err) throw err;
+    res.send(books);
+  });
+}
+
+// by id
+methods.get = (req, res, next) => {
+  Book.findById(req.params.id, (err, book) => {
+    if (err) throw err;
+    res.send(book)
+  });
+}
+
+methods.update = (req, res, next) => {
+  Book.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+  .exec((err, book) => {
+    if (err) throw err;
+    res.send(book);
+  });
+}
+
+methods.delete = (req, res, next) => {
+  Book.findByIdAndRemove(req.params.id)
+  .exec((err, book) => {
+    if (err) throw err;
+    res.send(book);
+  });
+}
+
+// GET ?title=blabla
+methods.findTitle = (req, res, next) => {
+  Book.find({ title: {$regex: req.query.title, $options: 'i'} }, (err, book) => {
+    if(err) throw err;
+    res.send(book);
+  });
+}
